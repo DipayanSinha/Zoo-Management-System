7c3eac5d25ecf3fd9ff9e4b2608de043d44936ba
DipayanSinha <romulus.sinha@gmail.com>
2018-11-09 09:31:50 +0530

    Version 2 commit

--- ZMS_Project/Zoo/views.py
+++ ZMS_Project/Zoo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import DepartmentForm,ExhibitForm,AnimalForm,StaffForm
-from .models import Animal,Department
+from .models import Animal,Department,Staff,Exhibit
 
 from django.shortcuts import get_list_or_404, get_object_or_404
 # Create your views here.
@@ -80,14 +80,47 @@ def add_staff(request):
     return render(request,'Zoo/add_Staff.html',context)
 
 def view_animals(request):
-    query_results = Animal.objects.all()
-    print(query_results)
-    context = {'query_results':query_results}
-    return render(request,'Zoo/list_animals.html',context)
+    animals = ""
+    if (request.method == 'POST'):
+        search_text = request.POST['search_text']
+    else:
+        search_text = ''
+    animals = Animal.objects.filter(commonname__contains=search_text) or Animal.objects.filter(id__contains=search_text)
+    return render(request, 'Zoo/list_animals.html', {'animals': animals})
+
+def animal_profile(request,id):
+    key = Animal.objects.get(pk=id)
+    context = {'key':key}
+    return render(request, 'Zoo/animal_profile.html', context)
+
+def animal_delete(request, id):
+    animal = Animal.objects.get(pk = id)
+    animal.delete()
+    messages.success(request, 'Deleted successfully')
+    return redirect('view_animals')
+
+def edit_animal(request, id):
+   animal = get_object_or_404(Animal, pk=id)
+   if request.method == "POST":
+       form = AnimalForm(request.POST, instance=animal)
+       if form.is_valid():
+          post = form.save(commit=True)
+          post.save()
+          return redirect('view_animals')
+   else:
+       form = AnimalForm(instance=animal)
+   return render(request, 'Zoo/edit_animal.html', {'form': form,'animal':animal})
 
 def deptList(request):
-    depts = Department.objects.all().order_by('id')
-    return render(request,'Zoo/department_list.html',{'depts':depts})
+    depts = ""
+    if (request.method == 'POST'):
+        search_text = request.POST['search_text']
+    else:
+        search_text = ''
+    depts = Department.objects.filter(name__contains=search_text) or Department.objects.filter(id__contains=search_text)
+    return render(request, 'Zoo/department_list.html', {'depts': depts})
+    #depts = Department.objects.all().order_by('id')
+    #return render(request,'Zoo/department_list.html',{'depts':depts})
 
 def delete(request, id):
     department = Department.objects.get(pk = id)
@@ -108,20 +141,81 @@ def edit_department(request, id):
        form = DepartmentForm(instance=department)
    return render(request, 'Zoo/edit_department.html', {'form': form,'dept':department})
 
-def search_department(request):
+'''def search_department(request):
     depts=""
     if(request.method=='POST'):
         search_text = request.POST['search_text']
     else:
         search_text=''
-    depts = Department.objects.filter(name__contains=search_text )
-    return render(request,'Zoo/department_search.html',{'depts':depts})
+    depts = Department.objects.filter(name__contains=search_text ) or Department.objects.filter(id__contains=search_text )
+    return render(request,'Zoo/department_search.html',{'depts':depts})'''
 
-
-def profile(request,id):
+def department_profile(request,id):
     key = Department.objects.get(pk=id)
     context = {'key':key}
-    return render(request,'Zoo/profile.html',context)
+    return render(request, 'Zoo/department_profile.html', context)
+
+def view_staff(request):
+    if (request.method == 'POST'):
+        search_text = request.POST['search_text']
+    else:
+        search_text = ''
+    staff = Staff.objects.filter(firstname__contains=search_text) or Staff.objects.filter(id__contains=search_text)
+    return render(request, 'Zoo/staff_list.html', {'staff': staff})
+
+def staff_profile(request,id):
+    key = Staff.objects.get(pk=id)
+    context = {'key':key}
+    return render(request, 'Zoo/staff_profile.html', context)
+
+def staff_delete(request, id):
+    staff = Staff.objects.get(pk = id)
+    staff.delete()
+    messages.success(request, 'Deleted successfully')
+    return redirect('staff_list')
+
+def edit_staff(request, id):
+   staff = get_object_or_404(Staff, pk=id)
+   if request.method == "POST":
+       form = StaffForm(request.POST, instance=staff)
+       if form.is_valid():
+          post = form.save(commit=True)
+          post.save()
+          return redirect('staff_list')
+   else:
+       form = StaffForm(instance=staff)
+   return render(request, 'Zoo/edit_staff.html', {'form': form,'staff':staff})
+
+def view_exhibit(request):
+    if (request.method == 'POST'):
+        search_text = request.POST['search_text']
+    else:
+        search_text = ''
+    exhibit = Exhibit.objects.filter(name__contains=search_text) or Exhibit.objects.filter(id__contains=search_text)
+    return render(request, 'Zoo/exhibit_list.html', {'exhibits': exhibit})
+
+def exhibit_profile(request,id):
+    key = Exhibit.objects.get(pk=id)
+    context = {'key':key}
+    return render(request, 'Zoo/exhibit_profile.html', context)
+
+def exhibit_delete(request, id):
+    exhibit = Exhibit.objects.get(pk = id)
+    exhibit.delete()
+    messages.success(request, 'Deleted successfully')
+    return redirect('exhibit_list')
+
+def edit_exhibit(request, id):
+   exhibit= get_object_or_404(Exhibit, pk=id)
+   if request.method == "POST":
+       form = ExhibitForm(request.POST, instance=exhibit)
+       if form.is_valid():
+          post = form.save(commit=True)
+          post.save()
+          return redirect('exhibit_list')
+   else:
+       form = ExhibitForm(instance=exhibit)
+   return render(request, 'Zoo/edit_exhibit.html', {'form': form,'exhibits':exhibit})
 
 def sidebar(request):
     return render(request,'Zoo/Sidebar.html',{})
